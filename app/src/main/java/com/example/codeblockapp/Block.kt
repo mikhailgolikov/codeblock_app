2c1bbc88c7c6c393c9f17bcdc6a1c0be5aad34ce
Mikhail Golikov <misha-golikov2006@mail.ru>
2025-05-20 19:19:10 +0700

    Добавление блоков на  поле для составления алгоритма

--- app/src/main/java/com/example/codeblockapp/Block.kt
+++ app/src/main/java/com/example/codeblockapp/Block.kt
@@ -1,74 +1,63 @@
 package com.example.codeblockapp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.math.roundToInt
+import androidx.compose.ui.geometry.Offset
 
-
-class Block {
-    var minWidth: Dp = 100.dp
-    var minHeight: Dp = 50.dp
-    var color: Color = Color.LightGray
-//    var modifier: Modifier = Modifier
-//    var mobile: Boolean = false
-//        get() = field
-//        set(value) {
-//            if (value) minWidth = 0.dp
-//        }
-
-
-
+data class Block(
+    val text: String = "Блок",
+    val color: Color = Color.LightGray,
+    val minWidth: Dp = 100.dp,
+    val minHeight: Dp = 50.dp
+) {
     @Composable
-    fun Create() {
-        var offset by remember { mutableStateOf(Offset.Zero)}
-        val state = rememberTransformableState {
-                scaleChange, offsetChange, rotationChange -> offset += offsetChange
+    fun Create(
+        initialOffset: Offset = Offset.Zero,
+        onOffsetChange: (Offset) -> Unit = {},
+        onClick: () -> Unit = {}
+    ) {
+        var offset by remember { mutableStateOf(initialOffset) }
+
+        val transformState = rememberTransformableState { _, offsetChange, _ ->
+            offset += offsetChange
+            onOffsetChange(offset)
         }
-        Box(modifier = Modifier
-            .graphicsLayer(
-                translationX = offset.x,
-                translationY = offset.y
-            )
-            .transformable(state = state)
-            .padding(10.dp)
-            .clip(shape = CircleShape)
-            .defaultMinSize(minWidth, minHeight)
-            .width(IntrinsicSize.Max)
-            .height(IntrinsicSize.Max)
-            .background(color = color),
+
+        Box(
+            modifier = Modifier
+//                .graphicsLayer(
+//                    translationX = offset.x,
+//                    translationY = offset.y
+//                )
+                .transformable(state = transformState)
+                .padding(10.dp)
+                .clip(CircleShape)
+                .defaultMinSize(minWidth, minHeight)
+                .width(IntrinsicSize.Max)
+                .height(IntrinsicSize.Max)
+                .background(color)
+                .clickable { onClick() },
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "something",
+            Text(
+                text = text,
                 fontSize = 20.sp,
                 modifier = Modifier.padding(horizontal = 20.dp),
+                color = Color.White
             )
         }
     }
--- app/src/main/java/com/example/codeblockapp/CodeScreen.kt
+++ app/src/main/java/com/example/codeblockapp/CodeScreen.kt
@@ -1,70 +1,57 @@
 package com.example.codeblockapp
 
-import android.graphics.drawable.GradientDrawable.Orientation
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showSystemUi = false, showBackground = true)
+
 @Composable
 fun CodeScreen() {
-    var color = remember{
-        mutableStateOf(Color(0xFF4CAF50))
-    }
-    var isRunning = remember{
-        mutableStateOf(false)
-    }
-    var text = remember{
-        mutableStateOf("▶")
-    }
-    var blocks = listOf(Block(), Block(), Block(), Block())
+    val color = remember { mutableStateOf(Color(0xFF4CAF50)) }
+    val isRunning = remember { mutableStateOf(false) }
+    val text = remember { mutableStateOf("▶") }
+
+    val availableBlocks = listOf(
+        Block(),
+        Block(),
+        Block(),
+        Block()
+    )
+
+    val algorithmBlocks = remember { mutableStateListOf<Block>() }
 
     Column {
-        Box (
-            modifier = Modifier.fillMaxSize().weight(1f),
-            contentAlignment = Alignment.Center
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Button (
+
+            Button(
                 onClick = {
                     if (isRunning.value) {
                         color.value = Color(0xFF4CAF50)
@@ -77,31 +64,61 @@ fun CodeScreen() {
                     }
                 },
                 colors = ButtonDefaults.buttonColors(
-                    contentColor = Color(0x00FFFFFF),
-                    containerColor = color.value),
+                    containerColor = color.value
+                ),
                 shape = CircleShape,
-                modifier = Modifier.padding(20.dp).size(50.dp).align(Alignment.TopEnd)
+                modifier = Modifier
+                    .padding(20.dp)
+                    .size(50.dp)
+                    .align(Alignment.TopEnd)
             ) {
                 Text(text.value, color = Color.White, fontSize = 24.sp, textAlign = TextAlign.Center)
             }
-            Text("Полe для блоков", fontSize = 16.sp)
+
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 60.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(algorithmBlocks) { block ->
+                    block.Create(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .size(200.dp, 60.dp)
+                    )
+                }
+            }
+
+
         }
+
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(150.dp)
                 .background(Color(0xFFFFE2D1)),
-
             contentAlignment = Alignment.Center
         ) {
-//            LazyRow(modifier = Modifier
-//                .fillMaxSize(),
-//                verticalAlignment = Alignment.CenterVertically
-//            ) {
-//                items(blocks) { block ->
-//                    block.Create()
-//                }
-//            }
+            LazyRow(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(availableBlocks) { block ->
+                    block.Create(
+                        modifier = Modifier
+                            //.size(100.dp, 50.dp)
+                                ,
+                        onClick = {
+                            algorithmBlocks.add(block.copy())
+                        }
+                    )
+                }
+            }
         }
     }
 }
